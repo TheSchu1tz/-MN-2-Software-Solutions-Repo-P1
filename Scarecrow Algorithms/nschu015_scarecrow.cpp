f5339d2a526e7d9bf79997b9c271cd999f17c08e
TheSchu1tz <noahjschultz@gmail.com>
2025-10-17 13:07:37 -0700

    Fixed some issues and almost finished search algorithm, need to fix stopping at 'Enter'

--- Scarecrow Algorithms/nschu015_scarecrow.cpp
+++ Scarecrow Algorithms/nschu015_scarecrow.cpp
@@ -4,6 +4,7 @@
 #include <random>
 #include <vector>
 #include <limits>
+#include <string>
 #include <cmath>
 
 using namespace std;
@@ -15,12 +16,6 @@ private:
     double y;
 public:
     Node(double newX, double newY) : x(newX), y(newY){};
-    void setX(double newX){
-        x = newX;
-    }
-    void setY(double newY){
-        y = newY;
-    }
     double getX(){
         return x;
     }
@@ -30,14 +25,13 @@ public:
 };
 
 //calculates distance between two Nodes
-double distance(Node a, Node b) {
+double distance(Node& a, Node& b) {
     return sqrt(pow((b.getX() - a.getX()),2)+pow((b.getY() - a.getY()),2));
 }
 
 //calculates total path distance of current Node organization
-double totalDistance(vector<Node> currPath) {
+double totalDistance(vector<Node>& currPath) {
     double result = 0;
-    Node pad = currPath[0];
     for (int i = 0; i < currPath.size() - 1; ++i){
         result += distance(currPath[i], currPath[i+1]);
     }
@@ -46,8 +40,25 @@ double totalDistance(vector<Node> currPath) {
 }
 
 //randomly shuffles order, replacing shortestDistance if new shortest is generated (outputs final shortest in place of eventual .txt output with all information)
-float searchUntilEnter(){
-    
+double searchUntilEnter(vector<Node>& Path){
+    double shortestDistance = INFINITY;
+    string inputLine;
+    random_device rd;
+    default_random_engine rng(rd());
+    while(1){
+        //break loop if enter pressed *FIX*
+        getline(cin, inputLine);
+        if (inputLine == "\n"){
+            cout << "Final shortest distance: " << shortestDistance << endl;
+            break;
+        }
+        shuffle(Path.begin() + 1, Path.end() - 1, rng);
+        double currDistance = totalDistance(Path);
+        if (currDistance < shortestDistance){
+            shortestDistance = currDistance;
+            cout << "       " << shortestDistance << endl;
+        }
+    }
 }
 
 int main() {
@@ -55,7 +66,6 @@ int main() {
     string fileName;
     ifstream inputFile;
     vector<Node> Path;
-    float shortestDistance = INFINITY;
 
     //get file name, open file, check if bad, take in all coords, count
     cout << "Enter the name of file: ";
@@ -64,27 +74,41 @@ int main() {
     if (!inputFile.is_open()){
         cout << "Error" << endl;
     }
-    float A, B;
+    double A, B;
     while (inputFile >> A >> B){
         Path.push_back(Node(A, B));
     }
     inputFile.close();
 
-    //begin outputting shortest paths
-    cout << "There are " << Path.size() << " nodes, computing shortest route.." << endl;
-    cout << "   Shortest Route Discovered So Far " << endl;
-
-    //Replace with function running until 'Enter' pressed
-    random_device rd;
-    default_random_engine rng(rd());
-    for (int i = 0; i < 100; ++i){
-        shuffle(Path.begin() + 1, Path.end() - 1, rng);
-        float currDistance = totalDistance(Path);
-        if (currDistance < shortestDistance){
-            shortestDistance = currDistance;
-            cout << "       " << shortestDistance << endl;
+    cout << "First Path Order: ";
+    for (int j = 0; j < Path.size(); ++j){
+        if (j < Path.size() - 1){
+            cout << "(" << Path[j].getX() << ", " << Path[j].getY() << ") to ";
+        }
+        else{
+            cout << "(" << Path[j].getX() << ", " << Path[j].getY() << ")";
         }
     }
-    
+
+    //begin outputting shortest paths
+    cout << endl << "There are " << Path.size() << " nodes, computing shortest route.." << endl;
+    cout << "   Shortest Route Discovered So Far " << endl;
+
+    //function runs until 'Enter' pressed, outputting 
+    double resultDistance = searchUntilEnter(Path);
+
+    cout << endl << "==========Results==========" << endl;
+    cout << "Shortest Path Distance: " << resultDistance << endl;
+    cout << "Shortest Path Order: ";
+    for (int j = 0; j < Path.size(); ++j){
+        if (j < Path.size() - 1){
+            cout << "(" << Path[j].getX() << ", " << Path[j].getY() << ") to ";
+        }
+        else{
+            cout << "(" << Path[j].getX() << ", " << Path[j].getY() << ")";
+        }
+    }
+    cout << "Shortest Path Image: *Image here*";
+
     return 0;
 }
